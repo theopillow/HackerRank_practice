b06f0d76ffc8dbf97d5766df7b68fc3da3abcff7
David Shin <theo7897@gmail.com>
2019-05-04 11:23:23 +0900

    rename

--- 30 Days of Code/Day 19 - Interfaces.py
+++ 30 Days of Code/Day 19 - Interfaces.py
@@ -5,13 +5,16 @@ class AdvancedArithmetic(object):
 
 class Calculator(AdvancedArithmetic):
     
+    # Best solution
     def divisorSum(self, n):
-        total = n
-        for i in range(1, n):
-            if n % i == 0:
-#                 print('i: {}'.format(i))
-                total += i
-        return total
+        return sum([x for x in range(1, n+1) if n % x == 0])
+    
+#     def divisorSum(self, n):
+#         total = n
+#         for i in range(1, n):
+#             if n % i == 0:
+#                 total += i
+#         return total
 
 
 n = int(input())
